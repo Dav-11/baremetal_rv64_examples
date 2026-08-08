410b8954eadbc0b3f8f391850e5f6dd0d09b34cf
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2026-08-08 12:41:03 +0200

    Added guard for core ID

--- src/add_entry.asm
+++ src/add_entry.asm
@@ -1,7 +1,14 @@
+.equ TARGET_HART, 0 # replace with core ID
+
 .section .text.entry_add
 .global _start_add
 
 _start_add:
+    # 0. Ensure only core TARGET_HART is running this code, else goto exit_sim
+    csrr a0, mhartid
+    li a1, TARGET_HART
+    bne a0, a1, exit_sim
+
     # 1. Set up the stack pointer
     .option push
     .option norelax
@@ -11,7 +18,10 @@ _start_add:
     # 2. Call main_add using jump-and-link
     jal main_add
 
-    # 3. Exit gem5 simulation
+    j exit_sim
+
+exit_sim:
+    # Exit gem5 simulation
     li a0, 0            # delay = 0
     .word 0x4200007b    # gem5 m5_exit instruction
 
--- src/select_entry.asm
+++ src/select_entry.asm
@@ -1,7 +1,14 @@
+.equ TARGET_HART, 1 # replace with core ID
+
 .section .text.entry_select
 .global _start_select
 
 _start_select:
+    # 0. Ensure only core TARGET_HART is running this code, else goto exit_sim
+    csrr a0, mhartid
+    li a1, TARGET_HART
+    bne a0, a1, exit_sim
+
     # 1. Set up the stack pointer
     .option push
     .option norelax
@@ -11,7 +18,10 @@ _start_select:
     # 2. Call main_select using jump-and-link
     jal main_select
 
-    # 3. Exit gem5 simulation
+    j exit_sim
+
+exit_sim:
+    # Exit gem5 simulation
     li a0, 0            # delay = 0
     .word 0x4200007b    # gem5 m5_exit instruction
 
